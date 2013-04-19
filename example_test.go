e86055f813bb1158844b9c11e0ad569dd2e13da4
Dan Frank <danielhfrank@gmail.com>
2013-04-19 11:56:32 -0400

    last try at examples

--- example_test.go
+++ example_test.go
@@ -1,13 +1,13 @@
-package hostpool_test
+package hostpool
 
 import (
 	"github.com/bitly/go-hostpool"
 )
 
-func Example_epsilon_greedy() {
+func ExampleNewEpsilonGreedy() {
 	hp := hostpool.NewEpsilonGreedy([]string{"a", "b"}, 0, &hostpool.LinearEpsilonValueCalculator{})
 	hostResponse := hp.Get()
 	hostname := hostResponse.Host()
-	err := _ // (make a request with hostname)
+	err := nil // (make a request with hostname)
 	hostResponse.Mark(err)
 }
